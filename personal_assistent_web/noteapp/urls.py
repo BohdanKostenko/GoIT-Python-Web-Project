9ed56027f2f02bc6956cb73d16514e5d2f5d1223
iMax <90639364+MelnykovMaksym@users.noreply.github.com>
2022-01-29 14:02:30 +0200

    add forms in noteapp

--- personal_assistent_web/noteapp/urls.py
+++ personal_assistent_web/noteapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.noteapp)
+    path('', views.noteapp, name='noteapp'),
+    path('create_note', views.create_note, name='create_note')
 ]
