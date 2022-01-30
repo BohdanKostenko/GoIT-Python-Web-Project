b6e85e2470bd63b8d8f1e3e59fe15f29452a9171
Bohdan Kostenko <77276415+BohdanKostenko@users.noreply.github.com>
2022-01-30 14:54:41 +0200

    add upload models

--- personal_assistent_web/addressbook/models.py
+++ personal_assistent_web/addressbook/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=32)
+    phone = models.CharField('phone', max_length=50)
+    birthday = models.DateField('birthday', default=date.today)
+    email = models.CharField('email', max_length=254)
+    address = models.CharField('address', max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
