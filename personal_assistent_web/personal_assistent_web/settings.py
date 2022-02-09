07bcee6c94f91e122a7be5bc15985e2fe3549bd1
Bohdan Kostenko <77276415+BohdanKostenko@users.noreply.github.com>
2022-02-09 18:32:43 +0200

    upload db settings

--- personal_assistent_web/personal_assistent_web/settings.py
+++ personal_assistent_web/personal_assistent_web/settings.py
@@ -89,10 +89,9 @@ WSGI_APPLICATION = 'personal_assistent_web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'web_project',
-        'USER': 'root',
-        'PASSWORD': '123456789',
-        'HOST': '127.0.0.1',
+        'NAME': 'web_project', 'USER': 'user1',
+        'PASSWORD': 'Python_project_goit1',
+        'HOST': '140.238.212.157',
         'PORT': '3306',
         'TIME_ZONE': 'Europe/Kiev',
         'OPTIONS': {'connect_timeout': 1},
