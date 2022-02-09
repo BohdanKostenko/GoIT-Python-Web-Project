424b5fdf4a322c6ea912cbfe77dcc202db84cd5a
Bohdan Kostenko <77276415+BohdanKostenko@users.noreply.github.com>
2022-02-09 18:33:46 +0200

    upload db settings

--- personal_assistent_web/personal_assistent_web/settings.py
+++ personal_assistent_web/personal_assistent_web/settings.py
@@ -79,13 +79,6 @@ WSGI_APPLICATION = 'personal_assistent_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
